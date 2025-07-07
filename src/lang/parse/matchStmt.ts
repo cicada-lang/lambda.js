abe78e595ad8baf0b645c14e4aaad222f501a81e
Xie Yuheng <xyheme@gmail.com>
2025-07-07 18:25:53 +0800

    up

--- src/lang/parse/matchStmt.ts
+++ src/lang/parse/matchStmt.ts
@@ -25,10 +25,6 @@ const stmtMatcher: X.Matcher<Stmt> = X.matcherChoice<Stmt>([
     return Stmts.Import(X.dataToString(url), entries.map(matchImportEntry))
   }),
 
-  X.matcher("(cons 'import exps)", ({ exps }) =>
-    Stmts.AssertEqual(X.dataToArray(exps).map(matchExp)),
-  ),
-
   X.matcher("(cons 'assert-equal exps)", ({ exps }) =>
     Stmts.AssertEqual(X.dataToArray(exps).map(matchExp)),
   ),
