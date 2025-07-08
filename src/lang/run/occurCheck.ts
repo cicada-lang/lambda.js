91ae491f89f250b06413aec0ddc42dd908a68e93
Xie Yuheng <xyheme@gmail.com>
2025-07-08 11:38:11 +0800

    ambr Definition Def

--- src/lang/run/occurCheck.ts
+++ src/lang/run/occurCheck.ts
@@ -1,27 +1,23 @@
 import dedent from "dedent"
-import type { Definition } from "../definition/Definition.ts"
+import type { Def } from "../def/Def.ts"
 import { expIndirectFreeNames } from "../exp/expIndirectFreeNames.ts"
 import * as Exps from "../exp/index.ts"
 import { formatExp } from "../format/formatExp.ts"
 import type { Mod } from "../mod/index.ts"
 
-export function occurCheck(mod: Mod, definition: Definition): void {
-  const indirectFreeNames = expIndirectFreeNames(mod, definition.exp)
+export function occurCheck(mod: Mod, def: Def): void {
+  const indirectFreeNames = expIndirectFreeNames(mod, def.exp)
 
-  if (!indirectFreeNames.has(definition.name)) return
+  if (!indirectFreeNames.has(def.name)) return
 
-  if (definition.exp.kind !== "Lambda") {
+  if (def.exp.kind !== "Lambda") {
     throw new Error(dedent`
       [occurCheck] Only function can be recursive.
 
-        non-function exp: ${formatExp(definition.exp)}
-        recursive name: ${definition.name}
+        non-function exp: ${formatExp(def.exp)}
+        recursive name: ${def.name}
       `)
   }
 
-  definition.exp = Exps.LambdaRec(
-    definition.name,
-    definition.exp.name,
-    definition.exp.ret,
-  )
+  def.exp = Exps.LambdaRec(def.name, def.exp.name, def.exp.ret)
 }
--- src/lang/run/runMod.ts
+++ src/lang/run/runMod.ts
@@ -1,4 +1,4 @@
-import { modOwnDefinitions, type Mod } from "../mod/index.ts"
+import { modOwnDefs, type Mod } from "../mod/index.ts"
 import { assertAllNamesDefined } from "./assertAllNamesDefined.ts"
 import { define } from "./define.ts"
 import { execute } from "./execute.ts"
@@ -9,11 +9,9 @@ export function runMod(mod: Mod): void {
 
   for (const stmt of mod.stmts) define(mod, stmt)
 
-  for (const definition of modOwnDefinitions(mod).values())
-    assertAllNamesDefined(mod, definition)
+  for (const def of modOwnDefs(mod).values()) assertAllNamesDefined(mod, def)
 
-  for (const definition of modOwnDefinitions(mod).values())
-    occurCheck(mod, definition)
+  for (const def of modOwnDefs(mod).values()) occurCheck(mod, def)
 
   for (const stmt of mod.stmts) {
     const output = execute(mod, stmt)
