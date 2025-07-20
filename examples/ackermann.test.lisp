5618a1101c9e7871c732fc759826ab53f968eaa7
Xie Yuheng <xyheme@gmail.com>
2025-07-20 19:48:23 +0800

    up

--- examples/ackermann.test.lisp
+++ examples/ackermann.test.lisp
@@ -1,9 +1,16 @@
 (import zero one two three "nat-church.lisp")
-(import ackermann "ackermann.lisp")
+(import ackermann ackermann-wrap "ackermann.lisp")
 
 (assert-equal (ackermann zero zero) one)
 (assert-equal (ackermann one zero) two)
 (assert-equal (ackermann zero one) two)
+
+ackermann-wrap
+
+;; TODO fail:
+
+;; ackermann
+;; (ackermann-wrap ackermann)
 
 ;; TODO fail:
 
