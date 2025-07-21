b52fef609f0f1c2a2626910bd09c86d244a2a884
Xie Yuheng <xyheme@gmail.com>
2025-07-22 01:43:11 +0800

    up

--- examples/factorial-wrap.test.lisp
+++ examples/factorial-wrap.test.lisp
@@ -3,9 +3,24 @@
 (import if true false "bool.lisp")
 (import Y turing "fixpoint.lisp")
 
-(import factorial-wrap factorial "factorial-wrap.lisp")
+(import factorial-wrap "factorial-wrap.lisp")
 
-;; test equivalence of functions
+(assert-equal ((Y factorial-wrap) zero) one)
+(assert-equal ((Y factorial-wrap) one) one)
+(assert-equal ((Y factorial-wrap) two) two)
+(assert-equal ((Y factorial-wrap) three) (mul three two))
+(assert-equal ((Y factorial-wrap) four) (mul four (mul three two)))
+
+(assert-equal ((turing factorial-wrap) zero) one)
+(assert-equal ((turing factorial-wrap) one) one)
+(assert-equal ((turing factorial-wrap) two) two)
+(assert-equal ((turing factorial-wrap) three) (mul three two))
+(assert-equal ((turing factorial-wrap) four) (mul four (mul three two)))
+
+(import factorial "factorial-wrap.lisp")
+
+(assert-equal factorial (factorial-wrap factorial))
+(assert-equal factorial (factorial-wrap (factorial-wrap factorial)))
 
 (assert-equal
   (lambda (factorial)
@@ -34,20 +49,27 @@
                 (sub1 n)))))
           (sub1 n)))))))
 
-(assert-equal ((Y factorial-wrap) zero) one)
-(assert-equal ((Y factorial-wrap) one) one)
-(assert-equal ((Y factorial-wrap) two) two)
-(assert-equal ((Y factorial-wrap) three) (mul three two))
-(assert-equal ((Y factorial-wrap) four) (mul four (mul three two)))
-
-(assert-equal ((turing factorial-wrap) zero) one)
-(assert-equal ((turing factorial-wrap) one) one)
-(assert-equal ((turing factorial-wrap) two) two)
-(assert-equal ((turing factorial-wrap) three) (mul three two))
-(assert-equal ((turing factorial-wrap) four) (mul four (mul three two)))
-
-(assert-equal (factorial zero) one)
-(assert-equal (factorial one) one)
-(assert-equal (factorial two) two)
-(assert-equal (factorial three) (mul three two))
-(assert-equal (factorial four) (mul four (mul three two)))
+(assert-equal
+  (factorial-wrap
+   (factorial-wrap
+    (factorial-wrap
+     factorial)))
+  (lambda (n)
+    (if (zero? n)
+      one
+      (mul
+       n
+       ((lambda (n)
+          (if (zero? n)
+            one
+            (mul
+             n
+             ((lambda (n)
+                (if (zero? n)
+                  one
+                  (mul
+                   n
+                   (factorial
+                    (sub1 n)))))
+              (sub1 n)))))
+        (sub1 n))))))
